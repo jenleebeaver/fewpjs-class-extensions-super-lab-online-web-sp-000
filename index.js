5615872139b7313694f6af5ecf8a8d7430627244
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-12-01 18:27:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your classes here
+// - A `Tree` instance should take in one parameter when created, `species`,
+//   and assign this to a property named `species`.
+// - A `Tree` should have a static method, `definition()`, that returns a short
+//   definition of all trees.
+//  
+// > A tree is a perennial plant with an elongated stem, or trunk, supporting
+// > branches and leaves.
